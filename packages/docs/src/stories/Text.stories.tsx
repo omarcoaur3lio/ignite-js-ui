605df4ea55bb0a31e2a49cdd8f2345f8c63db3fd
Marco Aurélio <sgtmarcoaurelio@icloud.com>
2024-08-04 20:40:25 -0300

    :sparkles: feat: Adicionar componente Text e sua documentação

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione enim ex minus vitae dolor natus consequuntur, perferendis fugiat incidunt eveniet, atque ipsa necessitatibus. Saepe iste cupiditate deleniti repellat doloribus itaque!',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
