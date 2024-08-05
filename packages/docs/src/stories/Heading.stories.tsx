fa29458f627049d6f787488898713e10cd809497
Marco Aurélio <sgtmarcoaurelio@icloud.com>
2024-08-04 21:04:26 -0300

    :sparkles: feat: Adicionar componente Heading e sua documentação

---
+++ packages/docs/src/stories/Heading.stories.tsx
@@ -0,0 +1,28 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Heading, HeadingProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Heading',
+  component: Heading,
+  tags: ['autodocs'],
+  args: {
+    children: 'Custom title',
+  },
+} as Meta<HeadingProps>
+
+export const Primary: StoryObj<HeadingProps> = {}
+
+export const CustomTag: StoryObj<HeadingProps> = {
+  args: {
+    children: 'H1 heading',
+    as: 'h1',
+  },
+  parameters: {
+    docs: {
+      description: {
+        story:
+          'Por padrão o heading sempre será um `H2`, isso pode ser alterado através da propriedade `as`',
+      },
+    },
+  },
+}
